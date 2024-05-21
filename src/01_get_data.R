3f6b177f7edc95a8ba205692aa4589ed2186c86f
mschoup <mspder@hotmail.com>
2024-05-21 14:31:23 +0000

    error in url

--- src/01_get_data.R
+++ src/01_get_data.R
@@ -1,5 +1,6 @@
 install.packages("tidyverse")
 library(tidyverse)
-d <- read_csv('https://tinyur1.com/gapcsv')
+d <- read_csv('https://tinyurl.com/gapcsv')
+#glimpse(d)
 write.csv(d, 'data/gapminder.csv')
 
